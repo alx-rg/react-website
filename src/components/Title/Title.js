a55b471f010d21c80df349a2c81c11c4388591aa
Alex Ross <alxrg@me.com>
2022-02-16 16:28:19 -0600

    Added the features

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -1,20 +1,23 @@
 
 import './Title.css'
 import {NavLink} from 'react-router-dom'
+import RandomSpace from '../RandomSpace/RandomSpace'
 
 const Title = () => {
   return (
     <div className="Title">
       <header>
-        <h1>SFPOPOS</h1>
-        <div className='Title-Subtitle'>San Francisco's Privately Owned Public Spaces</div>
-        <div>
+        <div className='Header-title'>
+          <h1>SFPOPOS</h1>
+        </div>
+        <div className='Header-container'>
           <NavLink 
             className={({ isActive }) => isActive ? "nav-link-active" : "nav-link" }
             to="/">List</NavLink>
           <NavLink 
             className={({ isActive }) => isActive ? "nav-link-active" : "nav-link" }
             to="/about">About</NavLink>
+          <RandomSpace />
         </div>
       </header>
     </div>
